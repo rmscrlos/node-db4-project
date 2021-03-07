b7a346a4cd96d9d442eaab8c7d56d2fc78c540f0
Carlos Ramos <rmscrlos@gmail.com>
2021-03-07 14:24:41 -0500

    create pk.json and installed dependencies. Also created folders and set up server.js and index.js

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).json({
+		message: 'Something went wrong. Please, try again later.'
+	});
+});
+
+module.exports = server;
